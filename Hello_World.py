720f6c0dda76a8ad6dafa43eddf4dd1e0e776fb7
PixelKyrin <57826563+PixelKyrin@users.noreply.github.com>
2020-02-01 01:44:00 +0900

    Update Hello_World.py
    add a font, font size, background color, font color,

--- Hello_World.py
+++ Hello_World.py
@@ -1,3 +1,5 @@
-import tkinter as tk
-win=tk.Tk()
-label=tk.Label(win,text="Hello World").pack()
+import tkinter 
+import tkinter.font
+win=tkinter.Tk()
+font=tkinter.font.Font(family="궁서체", size=20)
+label=tkinter.Label(win,text="Hello World", bg="hotpink", fg="skyblue", font=font, width=30, height=5).pack()
